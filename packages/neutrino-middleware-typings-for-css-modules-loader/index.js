6f6ed1e5af19c7566434ebb1fa2e1538bbacbc22
Lee Siong Chan <ahlee2326@me.com>
2017-10-29 00:15:38 +0800

    Update typings for css modules loader

--- packages/neutrino-middleware-typings-for-css-modules-loader/index.js
+++ packages/neutrino-middleware-typings-for-css-modules-loader/index.js
@@ -1,19 +1,26 @@
 const merge = require('deepmerge');
+const { WatchIgnorePlugin } = require('webpack');
 
 /*
   defaultOptions = {
-    css: {},
     ruleId: 'style',
     useId: 'css',
   };
 */
 module.exports = (neutrino, options = {}) => {
   neutrino.config
+    .plugin('watch-ignore')
+      .use(WatchIgnorePlugin, [[
+        /css\.d\.ts$/,
+      ]])
+      .end()
     .module
       .rule(options.ruleId || 'style')
         .use(options.useId || 'css')
           .loader(require.resolve('typings-for-css-modules-loader'))
-          .tap(opts => merge(opts, options.css, {
+          .tap(opts => merge(opts, {
+            camelCase: 1,
             modules: 1,
+            namedExport: 1,
           }));
 };
